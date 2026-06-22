f23c4fe68ecbf973d8f7c46324870d39e3eec961
PARAMKUMAR C PATEL <patelparam030@gmail.com>
2026-06-22 14:46:45 +0000

    Add BSTNode class for binary search tree

--- BSTNode.cs
+++ BSTNode.cs
@@ -1,1 +1,16 @@
+namespace SupplyChainManagementSystem
+{
+    public class BSTNode
+    {
+        public FurnitureItem Data { get; set; }
+        public BSTNode Left { get; set; }
+        public BSTNode Right { get; set; }
 
+        public BSTNode(FurnitureItem item)
+        {
+            Data = item;
+            Left = null;
+            Right = null;
+        }
+    }
+}
