9d99c3d60088106bffcf2dbd64dc2f2f92b93d47
PARAMKUMAR C PATEL <patelparam030@gmail.com>
2026-06-23 21:13:06 +0000

    Add Supplier class with properties

---
+++ Supplier.cs
@@ -0,0 +1,7 @@
+public class Supplier
+{
+    public int SupplierID { get; set; }
+    public string SupplierName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+}
